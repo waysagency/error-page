bb84b951f68b233e4fe42ec04221161ea86d530d
stijns96 <47820758+stijns96@users.noreply.github.com>
2023-09-01 16:35:08 +0200

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -6,6 +6,13 @@ const url = slug && slug.length > 0 ? slug.join("/") : "home";
 const story = await useAsyncStoryblok(url.replace(/\/$/, ""), {
   version: "draft",
 });
+
+if (story.value.status) {
+  throw createError({
+    statusCode: story.value.status,
+    statusMessage: story.value.response,
+  });
+}
 </script>
 
 <template>
